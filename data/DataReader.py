c95daa8516e11203f83c071c7b0024185d58d2d3
Nathan SUK <46647779+nathansuk@users.noreply.github.com>
2023-10-30 15:55:36 +0100

    getting csv from paris opendata api refacto

--- data/DataReader.py
+++ data/DataReader.py
@@ -1,20 +1,28 @@
 import pandas as pd
 import os
 import json
+import requests
 
 
 class DataReader:
     def __init__(self):
         # Get the path to /assets
-        current_dir = os.path.dirname(__file__)
-        csv_path = os.path.join(current_dir, '../assets/espaces_verts.csv')
-        geojson_path = os.path.join(current_dir, '../assets/espaces_verts.geojson')
+        self.current_dir = os.path.dirname(__file__)
+        self.polygons = self.set_geojson()  # self.polygons will contain all geometry points to draw
+        self.df = self.generate_df_from_api()
 
-        self.df = pd.read_csv(csv_path, sep=';', encoding='utf8')
-
-        # self.polygons will contain all geometry points to draw
+    def set_geojson(self):
+        geojson_path = os.path.join(self.current_dir, '../assets/espaces_verts.geojson')
         with open(geojson_path) as geojson_data:
-            self.polygons = json.load(geojson_data)
+            return json.load(geojson_data)
+
+    def generate_df_from_api(self):
+        api_endpoint = "https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/espaces_verts/exports/csv"
+        response = requests.get(api_endpoint)
+        with open('assets/datasets.csv', mode='wb') as file:
+            file.write(response.content)
+        csv_path = os.path.join(self.current_dir, '../assets/datasets.csv')
+        return pd.read_csv(csv_path, sep=';', encoding='utf8')
 
     def get_df(self):
         return self.df
--- data/GraphLib.py
+++ data/GraphLib.py
@@ -12,14 +12,14 @@ class GraphLib:
 
     # Return new pie chart showing space type repartition
     def create_pie_chart_space_type(self):
-        fig = px.pie(self.data_reader.df, names="Typologie d'espace vert")
+        fig = px.pie(self.data_reader.df, names="type_ev")
         return fig
 
     # Return new bar chart showing green spaces and their year when opened
     def create_bar_chart_year_open(self):
-        data = self.data_reader.df.dropna(subset=['Année de l\'ouverture'])
-        data = data[data['Année de l\'ouverture'] != 9999] # Deleting unusable data with year open equal to 9999
-        count_by_year = data['Année de l\'ouverture'].value_counts()
+        data = self.data_reader.df.dropna(subset=['annee_ouverture'])
+        data = data[data['annee_ouverture'] != 9999] # Deleting unusable data with year open equal to 9999
+        count_by_year = data['annee_ouverture'].value_counts()
         test = pd.DataFrame({'année': count_by_year.index, 'count': count_by_year.values})
         fig = px.bar(test, x='année', y='count')
         return fig
@@ -38,13 +38,13 @@ class GraphLib:
         map.save('carte.html')
 
     def create_bubble_charts_count(self):
-        if 'Code postal' in self.data_reader.df.columns:
+        if 'adresse_codepostal' in self.data_reader.df.columns:
             # Count green spaces / zip code
-            data = self.data_reader.df.groupby('Code postal').size().reset_index(name='Nombre d\'espaces verts')
+            data = self.data_reader.df.groupby('adresse_codepostal').size().reset_index(name='Nombre d\'espaces verts')
 
             # Delete zip code > 76000 (arbitrary) to focus only on Paris
-            data = data[data['Code postal'] <= 76000]
-            fig = px.scatter(data, x='Code postal', y='Nombre d\'espaces verts', size='Nombre d\'espaces verts', color='Nombre d\'espaces verts', hover_name='Code postal')
+            data = data[data['adresse_codepostal'] <= 76000]
+            fig = px.scatter(data, x='adresse_codepostal', y='Nombre d\'espaces verts', size='Nombre d\'espaces verts', color='Nombre d\'espaces verts', hover_name='adresse_codepostal')
 
             # Update x,y axes
             fig.update_xaxes(title_text='Code postal')
@@ -62,31 +62,31 @@ class GraphLib:
         df = self.data_reader.df
 
         # Keep only first word from 'Adresse type voie' ex : "RUE" because "RUE DE" and "RUE LA" were different
-        df['Adresse - type voie'] = df['Adresse - type voie'].str.split().str[0]
+        df['adresse_typevoie'] = df['adresse_typevoie'].str.split().str[0]
 
         # Delete the only "NON DEFINED"
-        df = df[df['Adresse - type voie'] != 'ND']
+        df = df[df['adresse_typevoie'] != 'ND']
 
         # Replace the only line with 'rue' by 'RUE'
-        df['Adresse - type voie'] = df['Adresse - type voie'].str.replace('rue', 'RUE')
+        df['adresse_typevoie'] = df['adresse_typevoie'].str.replace('rue', 'RUE')
 
         # Count occurrences for every type of street
-        street_type_counts = df['Adresse - type voie'].value_counts().reset_index()
+        street_type_counts = df['adresse_typevoie'].value_counts().reset_index()
 
-        street_type_counts.columns = ['Adresse - type voie', 'Count']
+        street_type_counts.columns = ['adresse_typevoie', 'Count']
 
         # Sort descending
         street_type_counts = street_type_counts.sort_values(by='Count', ascending=True)
 
-        fig = px.bar(street_type_counts, x='Count', y='Adresse - type voie',
+        fig = px.bar(street_type_counts, x='Count', y='adresse_typevoie',
                      title='Répartition des Types de Voies',
-                     labels={'Adresse - type voie': 'Type de Voie', 'Count': 'Nombre d\'Espaces Verts'}, )
+                     labels={'adresse_typevoie': 'Type de Voie', 'Count': 'Nombre d\'Espaces Verts'}, )
 
         return fig
 
     def create_heatmap_surface(self):
         # Select total surface for hhorticulture and perimeter to see correlations
-        correlations = self.data_reader.df[['Superficie totale réelle', 'Surface horticole', 'Périmètre']]
+        correlations = self.data_reader.df[['surface_totale_reelle', 'surface_horticole', 'perimeter']]
         correlations = correlations.corr()
 
         # Créez la heatmap avec Plotly Express
